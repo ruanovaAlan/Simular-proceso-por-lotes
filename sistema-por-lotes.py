bb6b5f821f0ed21f541afc2d52ac209069a39ced
Alan Ruanova <ruanovaalan96@gmail.com>
2024-02-07 17:12:01 -0600

    Initial commit

---
+++ sistema-por-lotes.py
@@ -0,0 +1,10 @@
+import time
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+#Frames
+content = ttk.Frame(root, borderwidth=10, relief='ridge' ,width=200, height=200)
+
+root.mainloop()

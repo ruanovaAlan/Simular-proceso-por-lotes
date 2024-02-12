00f0a50cf122fa593efab79dbdd01c216b49197a
Alan Ruanova <ruanovaalan96@gmail.com>
2024-02-12 17:25:04 -0600

    Add clock functionality to stop clock when no more processes

--- logic.py
+++ logic.py
@@ -4,7 +4,25 @@ from tkinter import *
 from tkinter import ttk
 
 start_time = None #Variable para el reloj
+clock_running = True #Variable para validar si el reloj esta corriendo
 lotes = [] #Array de lotes
+
+
+
+#Función que actualiza el reloj
+def update_clock(relojGlobal_label, root): 
+    global start_time
+    if start_time is None:
+        start_time = time.time()
+    if clock_running:
+        elapsed_time = time.time() - start_time
+        relojGlobal_label.config(text=f"Reloj: {int(elapsed_time)} segundos")
+        root.after(1000, update_clock, relojGlobal_label, root)  # Actualiza el reloj cada 1000 milisegundos
+
+#Funcion para detener el reloj
+def stop_clock():
+    global clock_running
+    clock_running = False
 
 #Funcion que retorna un numero aleatorio para el tiempo maximo estimado de un proceso
 def getTiempoMaxEstimado():
@@ -96,24 +114,30 @@ def en_espera(lotes, procesosEnEspera_text):
 def en_ejecucion(lotes, ejecucion_text, tiempo_inicio_proceso):
     lote_actual = lotes[0]  # Toma el primer lote
     procesoEnEjecucion = lote_actual[0]  # Toma el primer proceso en espera
+    
     if tiempo_inicio_proceso is None:  # Si es la primera vez que se llama a la función para este proceso
         tiempo_inicio_proceso = time.time() - start_time
+        
     tiempo_transcurrido = time.time() - start_time - tiempo_inicio_proceso
     tiempo_restante = procesoEnEjecucion['tiempo_maximo'] - tiempo_transcurrido
     ejecucion_text.delete('1.0', END) 
+    
     if lote_actual: #Muestra el proceso en ejecución
         ejecucion_text.insert(END, f"{procesoEnEjecucion['numero_programa']}. {procesoEnEjecucion['nombre']}\n{procesoEnEjecucion['operacion']}\nTME: {round(tiempo_restante) if tiempo_restante > 0 else 0}")
     return tiempo_restante, tiempo_inicio_proceso
 
 def terminados(lotes, terminados_text, procesos_terminados, tiempo_restante, tiempo_inicio_proceso, ejecucion_text, obtenerResultadosBtn):
     lote_actual = lotes[0]
-    if tiempo_restante <= 0:  
+    
+    if tiempo_restante <= 1:  #? cambiar el tiempo a 0
         procesos_terminados.append(lote_actual.pop(0))  # Elimina el proceso de la lista de procesos en espera y lo añade a la lista de procesos terminados
         tiempo_inicio_proceso = None  # Resetea el tiempo de inicio para el próximo proceso
         if not lote_actual:  # Si el lote actual está vacío
             lotes.pop(0)  # Elimina el lote de la lista de lotes
             ejecucion_text.delete('1.0', END)
+            
     terminados_text.delete('1.0', END)
+    
     for proceso in procesos_terminados: #Muestra los procesos terminados
         resultado = round(eval(proceso['operacion']), 4)
         terminados_text.insert(END, f"{proceso['numero_programa']}. {proceso['nombre']}\n{proceso['operacion']} = {resultado}\n\n")
@@ -121,6 +145,7 @@ def terminados(lotes, terminados_text, procesos_terminados, tiempo_restante, tie
     # Si todos los lotes están vacíos, habilita el botón obtenerResultadosBtn
     if not lotes:
         obtenerResultadosBtn.config(state='normal')
+        stop_clock() #Detiene el reloj si no hay más procesos
     
     return tiempo_inicio_proceso
 
@@ -136,15 +161,6 @@ def ejecutar_proceso(lotes, noLotesPendientes_label, ejecucion_text, root, proce
         noLotesPendientes_label.config(text=f"# De lotes pendientes: {cantidad_lotes}")
         # Llama a la función de nuevo después de 1000 milisegundos
         root.after(1000, ejecutar_proceso, lotes, noLotesPendientes_label, ejecucion_text, root, procesosEnEspera_text, terminados_text, obtenerResultadosBtn, procesos_terminados, tiempo_inicio_proceso)
-
-#Función que actualiza el reloj
-def update_clock(relojGlobal_label, root): 
-    global start_time
-    if start_time is None:
-        start_time = time.time()
-    elapsed_time = time.time() - start_time
-    relojGlobal_label.config(text=f"Reloj: {int(elapsed_time)} segundos")
-    root.after(1000, update_clock, relojGlobal_label, root)  # Actualiza el reloj cada 1000 milisegundos
 
 
 #Funcion para generar procesos y ejecutarlos
